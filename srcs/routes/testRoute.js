ae0ca0b816baf8a0b1ad0719f356fd5768f20565
ParkSeonGyun <qkrtjsrbs315@gmail.com>
2024-05-27 19:46:11 +0900

    Feat : tmp files

--- srcs/routes/testRoute.js
+++
@@ -1,7 +0,0 @@
-//testRoute.js
-import { test } from '../controllers/testController.js';
-import express from 'express';
-
-export const testRouter = express.Router();
-
-testRouter.get('/test', test);
